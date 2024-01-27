758cceb30bfb8acd1325998143a952d774b22ad9
Dambar Sing <programmer.gshi18@gmail.com>
2024-01-27 11:37:29 +0545

    Learning Nodejs

---
+++ Learn NodeJs/app.js
@@ -0,0 +1,15 @@
+//forIn loop
+// var person = {
+//     age: 12,
+//     name: "hai"
+// };
+// for(var key in person){
+//     // person[key];
+// }
+// console.log(key, person[key])
+
+//forEach loop
+var a =[1,4,3]
+a.forEach(function(val){
+       console.log(val+2);
+    })
