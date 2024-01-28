6ae92f8ebfa43a648819b18faf191fb5fceada0f
Dambar Sing <programmer.gshi18@gmail.com>
2024-01-28 23:09:49 +0545

    Installing nodemon, postman and created GET API

--- Learn NodeJs/app.js
+++ Learn NodeJs/app.js
@@ -4,12 +4,16 @@ const dambar = require("express");
 const app = dambar();
 
 app.get("/", (req,res)=>{
-    res.send("Hi, I am Dambar Sing");
+    res.json({
+        name: "Dambar",
+        age: 19,
+        address: "Nepalgunj"
+    })
 })
 app.get("/dambar", (req,res)=>{
     res.send("You are interested on Dambar, right?");
 })
 
 app.listen(2000, (req,res)=>{
-    console.log("Hi, I am Node JS")
+    console.log("NodeJs has started at port 2000")
 })
