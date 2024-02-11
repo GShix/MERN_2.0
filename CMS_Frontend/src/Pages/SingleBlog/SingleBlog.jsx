3922bbbb28b6ab0d328c2122a652cd3d07ce7c19
Dambar Sing <programmer.gshi18@gmail.com>
2024-02-11 22:44:53 +0545

    fetching singleBlog API and rendring  in title,subTitle & description

--- CMS_Frontend/src/Pages/SingleBlog/SingleBlog.jsx
+++ CMS_Frontend/src/Pages/SingleBlog/SingleBlog.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const SingleBlog = () => {
-    const {id} = useParams();
-    console.log(id)
+    // const id = useParams();
+    const {id} = useParams(); //Dynamic Route ko id lina laai
+    const [blog,setBlog] = useState({});
+    const fetchSingleBlog = async()=>{
+       const response= await axios.get('http://localhost:3000/blogs/'+id);
+    //    console.log(response)
+        if(response.status==200){
+            setBlog(response.data.blogs)
+        }
+    }
+    useEffect(()=>{
+        fetchSingleBlog()
+    },[])
+    // console.log(blog,'Blogs')
   return (
     <div>
-        <h1>Single Blog</h1>
+        <h1>{blog.title}</h1>
+        <h2>{blog.subTitle}</h2>
+        <p>{blog.description}</p>
     </div>
   )
 }
